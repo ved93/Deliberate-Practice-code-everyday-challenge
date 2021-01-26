3d8a485327304637e063eefe33575355d04b0dcc
ved93 <vpved93@gmail.com>
2021-01-26 10:00:22 +0530

    day 71

---
+++ code-everyday-challenge/n71_binary_knapsack.py
@@ -0,0 +1,21 @@
+
+
+def main(max_weight,weights,values):
+    
+
+    return 1
+
+
+if __name__ == "__main__":
+    # n , max_w = map(int,input().split())
+    # weights = []
+    # values = []
+    # for _ in range(n):
+    #     w,v=map(int,input().split())
+    #     weights.append(w)
+    #     values.append(v)  
+    # print(weights , values)
+    weights = [1, 1, 1]
+    values = [3, 2, 1]
+    max_weight = 6
+    print(main(max_weight,weights,values))
